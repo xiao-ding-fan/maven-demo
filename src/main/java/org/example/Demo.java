93d61a764372fc08313b11b46307fc976ff7451a
小叮忠实粉丝 <1186938260@qq.com>
2022-04-26 23:29:33 +0800

    init commit

---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,11 @@
+﻿package org.example;
+
+/**
+ * @author xiaowangtongxue
+ * @date 2022/4/26 23:22
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
